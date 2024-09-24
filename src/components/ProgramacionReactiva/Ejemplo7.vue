ba572065c1fcf1cb498e9974f2f8c7b0831fd78b
Lautaro Montaña <joselautarom@gmail.com>
2024-09-24 08:33:58 -0300

    Ejemplo 7

---
+++ src/components/ProgramacionReactiva/Ejemplo7.vue
@@ -0,0 +1,34 @@
+<!-- Ejemplo 7: v-bind -->
+
+<template>
+    <div>
+        <!-- Aplicamos estilos dinamicos al párrafo -->
+         <p :style="{color: textoColor, fontSize: fontSize + 'px'}">
+            Texto con estilo dinámico
+         </p>
+        <button @click="changeColor">Cambiar de color</button>
+        <button @click="changeFontSize"> Cambiar de tamaño</button>
+    </div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+
+const textoColor = ref('black')
+
+const fontSize = ref(16)
+
+// Funcion para cambiar el tamaño de la fuente
+const changeColor = () =>{
+    // Alternamos entre negro y rojo
+    textoColor.value = textoColor.value === 'black' ? 'red' : 'black'
+}
+
+// Función para cambiar el tamaño de la fuente
+const changeFontSize = () =>{
+    // Incrementamos el tamaño en dos px
+    fontSize.value += 2
+    // Si supera 24 px, volvemos a 16px
+    if(fontSize.value > 24) fontSize.value = 16
+}
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 // import HomeView from '../views/HomeView.vue'
 import App from '@/App.vue'
-import Ejemplos from '@/components/ProgramacionReactiva/Ejemplo6.vue'
+import Ejemplos from '@/components/ProgramacionReactiva/Ejemplo7.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
