88f6eb0d3c81a69ed1250f0df6f902f8390b24ac
Lautaro Montaña <joselautarom@gmail.com>
2024-09-24 06:44:32 -0300

    Ejemplo 3

---
+++ src/components/ProgramacionReactiva/Ejemplo3.vue
@@ -0,0 +1,24 @@
+<!-- Ejemplo 3: formulario reactivp -->
+ <template>
+    <!-- Agregamos un evento de tipo submit el cual va a ser el envio del form -->
+     <!-- .prevent el comportamiento por defecto del formulario -->
+    <form @submit.prevent="submitForm">
+        <input v-model="formData.name" type="text" placeholder="nombre">
+        <input v-model="formData.email" type="email" placeholder="email">
+        <button type="submit">Enviar</button>
+    </form>
+ </template>
+
+<script setup>
+    import { reactive } from 'vue';
+
+    // Creamos un objeto reactivo para los datos del formulario
+    const formData = reactive({
+        name: '',
+        email: ''
+    })
+
+    const submitForm = () =>{
+        console.table('Formulario enviado: ', formData)
+    }
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 // import HomeView from '../views/HomeView.vue'
 import App from '@/App.vue'
-import Ejemplos from '@/components/ProgramacionReactiva/Ejemplo2.vue'
+import Ejemplos from '@/components/ProgramacionReactiva/Ejemplo3.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
