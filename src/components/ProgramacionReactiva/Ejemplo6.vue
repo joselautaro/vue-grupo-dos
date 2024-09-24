3a826058a8d5534310c6ec8c69cdbe1012a1df10
Lautaro Montaña <joselautarom@gmail.com>
2024-09-24 07:33:58 -0300

    Ejemplo 6

---
+++ src/components/ProgramacionReactiva/Ejemplo6.vue
@@ -0,0 +1,26 @@
+<!-- Ejemplo 6: Uso de v-if y v-else -->
+
+<template>
+
+    <div>
+        <!-- Boton para alternar la visbilidad -->
+         <button @click="toggleVisibility">Mostrar u ocultar</button>
+         <!-- Este párrafo se muestra solo si isVisible es true -->
+          <p v-if="isVisible">Este texto es visible</p>
+          <!-- Este parrafo se muestra si isVisible es false -->
+           <p v-else>El texto está oculto</p>
+    </div>
+
+</template>
+
+<script setup>
+import {ref} from 'vue'
+
+const isVisible = ref(true)
+
+// Funcion para alternar la visibilidad
+const toggleVisibility = () =>{
+    // invertimos el valor de isVisible
+    isVisible.value = !isVisible.value
+}
+</script>
