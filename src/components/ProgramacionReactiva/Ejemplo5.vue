5600fdc7a27b1c1550747ccf1163d725d844ab30
Lautaro Montaña <joselautarom@gmail.com>
2024-09-24 07:25:10 -0300

    Ejemplo 5

---
+++ src/components/ProgramacionReactiva/Ejemplo5.vue
@@ -0,0 +1,30 @@
+<!-- Propiedades computadas -->
+ <template>
+    <div>
+        <!-- Inputs vinculados a firstName y lastName -->
+         <input type="text" v-model="firstName">
+         <input type="text" v-model="lastName">
+         <p>Nombre completo: {{ fullName }}</p>
+    </div>
+ </template>
+
+ <script setup>
+ import {ref, computed} from 'vue'
+
+//  Referencias reactivas para el nombre y apellido
+const firstName = ref('');
+const lastName = ref('');
+
+// Propiedades computadas para el nombre completo
+const fullName = computed(() =>{
+    // Concatenamos firstName y lastName, eliminando espacios extras
+    return `${firstName.value} ${lastName.value}`.trim()
+})
+
+</script>
+
+<style scoped>
+input{
+    margin: 10px;
+}
+</style>
